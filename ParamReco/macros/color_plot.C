082024d47976aeba46fefafaad75a86af79b2b1e
atishelmanch <abe2323@yahoo.com>
2018-05-22 16:44:30 -0400

    Updated Color_Plot macro

--- ParamReco/macros/color_plot.C
+++ ParamReco/macros/color_plot.C
@@ -3,9 +3,9 @@ void color_plot(){
   // Want to plot EB reconstructed amplitude as a function of iEta and iPhi 
   time_t initial = time(0); // initial time to calculate total time 
 
-  const int max_rows = -1; // < 0 to read all rows 
-
-  double time_shift = +7.0; // Positive/Negative: shift waveform to Left/Right
+  // File Parameters
+  const int max_rows = 1000; // < 0 to read all rows 
+  double time_shift = 0.0; // Positive/Negative: shift waveform to Left/Right
 
   // Set conditions 
   bool EE_Only = true; // Skip to row 60495 of crystal_parameters.txt
@@ -14,25 +14,18 @@ void color_plot(){
   bool side_filled = false;
 
   ostringstream sss1, sss2;
-  sss1 << "Recon Amp / Peak Samp, - side," << " time_shift = " << time_shift << "ns"; 
-  sss2 << "Recon Amp / Peak Samp, + side," << " time_shift = " << time_shift << "ns"; 
+  sss1 << "Recon Amp / A, - side," << " time_shift = " << time_shift << "ns"; 
+  sss2 << "Recon Amp / A, + side," << " time_shift = " << time_shift << "ns"; 
   TString histo_title1 = sss1.str();
   TString histo_title2 = sss2.str();
 
   TH2F *amps1 = new TH2F("-",histo_title1,100,0,100,100,0,100);
   TH2F *amps2 = new TH2F("+",histo_title2,100,0,100,100,0,100);
 
-  // Fill histo with (iEta, iPhi, reconamp)
+  // Fill histos with (ix, iy, Ahat / A)
 
-  // Each eta has 360 phi 
-  // ieta range is -85 to 85, from negative z side to positive z 
-
-  // ieta[0] => ieta = -1 ... ieta[84] => ieta = -85 ... ieta[85] => ieta = 1 ... ieta[169] => ieta = 85
-  // this is currently useful as it's the order of eta's in the current data text file. ** or may not be, need to verify that
-
-  // Create x and y arrays 
-  // Barrel: 838861313 - 838970216 inclusive
-  // Endcap: 838970216 and up
+  // Barrel: 838861313 <= ID <= 838970216 
+  // Endcap: ID >= 872415401 
 
   // Open File
 
@@ -169,8 +162,6 @@ void color_plot(){
 
 	  // Calculate Amplitude 
 
-          // /*
-
 	  Double_t A = d2, t_0 = d3, alpha = d4, beta = d5; 
 
 	  if (normalized_A == true){
@@ -183,16 +174,9 @@ void color_plot(){
 	      t_0 = 125.0; // 125 ns
 	      }
 
-	  // introduce time shift in formula. I guess just set t0 -> t0 + time_shift
-	  // time_shift
-
 	  TString *name = new TString("function_alphabeta");
 	  TString *formula = new TString("( (1 + (x-[1]) / ([2]*[3]) ) > 0.0)*([0] * pow( 1 + (x-[1]) / ([2]*[3]) , [2] ) * exp ( - (x-[1]) / [3])) + ((1 + (x-[1]) / ([2]*[3]) <= 0.0 ))*(0)");
 
-	  //double dt = alpha*beta/2.0;
-	  //double xmin = (-1.0)*alpha*beta + t_0 + dt; // time of first non-imaginary value + dt
-	  //double xmin = t_0 - dt;
-	  //double xmax = t_0 + 4*dt;
 	  double xmin = 100.; // Start Sampling at 100ns
 	  double xmax = 250.; // End Sampling at 250ns 
 	  double dt = 25;
@@ -205,7 +189,7 @@ void color_plot(){
 	  function_alphabeta->SetParameter (3, beta);  
 
 	  // weights 
-	  double w[10] = {-0.3812788, -0.3812788, -0.3812788, 0, 0.235699, 0.4228363, 0.3298652, 0.1575187, -0.002082776, 0}; // EB weights 
+	  double w[10] = {-0.3812788, -0.3812788, -0.3812788, 0, 0.235699, 0.4228363, 0.3298652, 0.1575187, -0.002082776, 0}; // CMSSW weights (just EB? Should update by calculating weights with EB EE waveforms on CMSSW) 
 	  int samp_number = 4; // assuming first four samples are zero (pedestal) and problem with -nan fourth sample. Also, fourth weight is zero so this sample isn't considered anyway.
 
 	  // Initialize Samples
@@ -216,13 +200,15 @@ void color_plot(){
 	    }
 
 	  // Extract Samples From Waveform
+
 	  for(double j = xmin; j < xmax; j += dt){
 
 	    samples[samp_number] = function_alphabeta->Eval(j);
 	    samp_number++ ;    
 	    }
 
-	  // Calculate Amplitude
+	  // Sum samples * weights
+
 	  for (int k = 0; k < 10; k++ ){
 
 	    recon_amp += samples[k]*w[k]; 
@@ -232,13 +218,9 @@ void color_plot(){
 	  double ratio = recon_amp / d2;
 	  //cout << "ratio = " << ratio << "\n" << endl;
 
-	  if (ratio < min){ min = ratio;}
-	  if (ratio > max){ max = ratio;}
-
-	  //amps->Fill(iphi,ieta,recon_amp);
-	  //amps->Fill(1,1,1.0);
-
-          //  */        
+	  //if (ratio < min){ min = ratio;}
+	  //if (ratio > max){ max = ratio;}
+    
 	  if (!side_filled){ amps1->Fill(ix,iy,ratio); }
 	  if (side_filled){ amps2->Fill(ix,iy,ratio); }
 
@@ -263,95 +245,10 @@ void color_plot(){
   inparamFile.close();
   inFile.close();
 
-  //cout << "valcount = " << valcount << "\n" << endl;
-  //int current_eta = -85;
-
-  // Create ieta_vals array to plot avg amp vs ieta_vals
-
-  /*
-
-
-  bool reached_zero = false;
-
-  for (int i = 0; i < 170; i++){
-
-      if (i == 85){
-
-	  reached_zero = true;
-	  
-	}
-
-      ieta_vals[i] = (-85 + i + reached_zero);
-      //current_eta += 1; 
-    }
-
-  // Want to create average array from vals.
-  //int zero_vals = 0;
-  int value_counter[170] = {0};
-
-  reached_zero = false;
-	
-  for (int i = 0; i < 170; i++){
-
-      if (i == 85){
-
-	  reached_zero = true;
-	  
-	}
-
-    for (int a = 0; a < valcount; a++){ // number of amplitudes 
-
-      if ( (xval[a] == (-85 + i + int(reached_zero))) && (yval[a] != 0) ) {
-	    //cout << "in loop\n" << endl;
-	    avg_amp[i] += yval[a]; 
-	    value_counter[i] += 1; 
-	    //cout << "value_counter[" << i << "] = " << value_counter[i] << "\n" << endl;
-            
-	  }
-	  //cout << "value_counter[" << i << "] = " << value_counter[i] << "\n" << endl;
-	  //if (int(yval[a]) == 0){
-
-	   // zero_vals += 1;
-	  //}
-
-
-  } // values
-
-  if (value_counter[i] != 0){
-
-      avg_amp[i] /= value_counter[i];
-      cout << "avg_amp[" << i << "] = " << avg_amp[i] << "\n" << endl;
-    }
-
-  } // etas
-
-
- */
-
   // Plot
 
-  //TMultiGraph *mg = new TMultiGraph();
-  //mg->Update();
-  //mg->SetTitle("Reconstructed Amplitude vs. iEta; iEta; Recon. Amp."); // Don't know why this isn't working 
-  //mg->SetTitle("Raw Amp vs. iEta");
-
-  //TGraph* gr = new TGraph(valcount,xval,yval);
-  //TGraph* avg = new TGraph(170,ieta_vals,avg_amp);
-  //gr->GetYaxis()->SetRangeUser(0.9,1.1);
-  //gr->GetXaxis()->SetRangeUser(-85,85);
-  //gr->SetTitle(etatitle_);
-  //gr->SetMarkerColor(1); // Black
-  //gr->SetMarkerStyle(1);
-
-  //avg->SetMarkerStyle(7);  // marker customization is for scatter plots
-  //avg->SetMarkerColor(4); // line ' ' for line plots 
-  //avg->SetLineColor(2);
-  //avg->SetLineStyle(9);
-  //gr->SetMarkerSize(1);
-
-  //mg->Add(horzline);
-  //mg->Add(gr,"AP"); // scatter plot, draw axis where scatter plot data is 
-  //mg->Add(avg,"P"); // line plot 
+  // Color axis limits
+  double lower = 0.99, upper = 1.01;
 
   TCanvas *c1 = new TCanvas("c1","c1",800,600);
 
@@ -360,8 +257,11 @@ void color_plot(){
   c1->Update();
 
   //amps1->GetZaxis()->SetRangeUser(min,max);
-  amps1->GetZaxis()->SetRangeUser(0.95,1.05);
+  amps1->GetZaxis()->SetRangeUser(lower,upper);
+  //c1->SetBatch(kFALSE);
+
   amps1->Draw("COLZ");
+  amps1->GetZaxis()->SetLabelSize(0.02);
 
   cout << "min = " << min << "\n" << endl;
   cout << "max = " << max << "\n" << endl;
@@ -373,32 +273,8 @@ void color_plot(){
   amps2->Draw();
   c2->Update();
  
-  amps2->GetZaxis()->SetRangeUser(0.99,1.02);
+  amps2->GetZaxis()->SetRangeUser(lower,upper);
   amps2->Draw("COLZ");
-
-  //TLegend *legend = new TLegend(0.1,0.7,0.48,0.9);
-  //legend->SetHeader("Legend","C"); // option "C" allows to center the header
-  //legend->AddEntry(h1,"Histogram filled with random numbers","f");
-  //legend->AddEntry("f1","Function abs(#frac{sin(x)}{x})","l");
-  //legend->AddEntry("gr","Graph with error bars","lep");
-  //legend->AddEntry(gr,"Data","p");
-  //legend->AddEntry("avg","Average","l");
-  //legend->AddEntry("fitfunc","Fit","l");
-  //gStyle->SetOptStat(kTRUE);
-  //legend->Draw();
-
-  //gStyle->SetOptStat(kFALSE);
-  //mg->Draw();
-  
-  //c1->Update();
-  //TLine *horzline = new TLine(c1->GetUxmin,1.0,c1->GetUxmax,1.0); // xmin, ymin, xmax, ymax 
-  //TLine *horzline = new TLine(-85,1.0,85,1.0); // xmin, ymin, xmax, ymax 
-  //horzline->SetLineColor(2);
-  //horzline->SetLineStyle(9);
-  //horzline->Draw();
-
-  //c1->BuildLegend();
-  // TGraphPolar for phi plots 
 
   time_t result = time(0); // save with time since epoch to avoid overwriting files
 
@@ -410,8 +286,8 @@ void color_plot(){
   TString plot_title1 = oss1.str();
   TString plot_title2 = oss2.str();
 
-  c1->SaveAs(plot_title1);
-  c1->SaveAs(plot_title2);
+  //c1->SaveAs(plot_title1);
+  //c1->SaveAs(plot_title2);
 
   time_t difference = result - initial;
   double minutes = double(difference) / 60.;
