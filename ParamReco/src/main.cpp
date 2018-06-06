09c526bc1a5494737288db9fa5f88c36c996532b
atishelmanch <abe2323@yahoo.com>
2018-06-06 12:20:23 -0400

    Updated Recon Amp Plotter Currently updating the EE/EB plotter to compute reconstructed amplitude using ideal weights for a given XTAL. This means implementing the ComputeWeights class.

--- ParamReco/src/main.cpp
+++ ParamReco/src/main.cpp
@@ -4,8 +4,30 @@
 // The purpose of this main function is to plot reconstructed amplitude / A parameter 
 // for EB and EE with desired time shift and weights.
 
+// Currently using this to compile:
+// g++ -std=c++11 -o test.exe main.cpp -lCLHEP `root-config --ldflags --glibs --cflags`
+
+using namespace std;
+
+// Include root header files
+#include "TString.h"
+#include "TH2.h"
+#include "TF1.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <iterator>
+#include <string>
+#include <chrono>
+
+// files you're going to need in this here main file.
+#include "ComputeWeights.h"
 #include "fill_histograms.cpp" 
-#include "recon_amp.cpp"
 #include "plot.cpp"
 
 int main()
@@ -14,7 +36,7 @@ int main()
 
 	// Set Study Parameters
 	double time_shift = 0; // Move waveform +/- ns to right/left
- 	int max_rows = -1; // < 0 to read all rows of XTAL_Params.txt
+ 	int max_rows = 3; // < 0 to read all rows of XTAL_Params.txt
 
 	// Define weights 
 	//double w[10] = {-0.3812788, -0.3812788, -0.3812788, 0, 0.235699, 0.4228363, 0.3298652, 0.1575187, -0.002082776, 0};
@@ -22,13 +44,19 @@ int main()
 	double EE_w[10] = {0, 0, -0.65, -0.52, 0.25, 0.52, 0.50, 0, 0, 0}; // Endcap
 
   	// Set conditions 
-	bool EB_Only = false;
-  	bool EE_Only = false; // Skip to row 60495 of crystal_parameters.txt
+	bool EB_Only = false; // Maybe change to fill EB and fill EE...
+  	bool EE_Only = true; // Skip to row 60495 of crystal_parameters.txt
   	bool normalized_A = false;
   	bool normalized_t0 = false; 
 
 	fill_histograms(max_rows, time_shift, EB_w, EE_w, EB_Only, EE_Only, normalized_A, normalized_t0);
+	//plot(zmin,zmax);
 
+	//bool EB_Only = true;
+  	//bool EE_Only = false;
+
+	//fill_histograms(max_rows, time_shift, EB_w, EE_w, EB_Only, EE_Only, normalized_A, normalized_t0);
+	//plot(zmin,zmax);
 	// Plotting parameters
 	
 	double zmin = 0.99;
--- ParamReco/src/recon_amp.cpp
+++ ParamReco/src/recon_amp.cpp
@@ -3,13 +3,21 @@
 // 
 // The purpose of this function is to calculate reconstructed amplitude 
 
+//#include <ComputeWeights.h>
+#include "ComputeWeights.cpp"
+
+//#include "TString.h"
+
+//#include "CLHEP/Matrix/Matrix.h"
+//#include "CLHEP/Matrix/SymMatrix.h"
+
 double recon_amp(double A, double t_0, double time_shift, double alpha, double beta, double weights[])
 {
 
 	TString *name = new TString("function_alphabeta");
 	TString *formula = new TString("( (1 + (x-[1]) / ([2]*[3]) ) > 0.0)*([0] * pow( 1 + (x-[1]) / ([2]*[3]) , [2] ) * exp ( - (x-[1]) / [3])) + ((1 + (x-[1]) / ([2]*[3]) <= 0.0 ))*(0)");
 
-	double xmin = 0; // Start Sampling at 100ns
+	double xmin = 0; // Start Sampling at 0ns
 	double xmax = 250.; // End Sampling at 250ns 
 	double dt = 25;
 
@@ -20,6 +28,63 @@ double recon_amp(double A, double t_0, double time_shift, double alpha, double b
 	function_alphabeta->SetParameter (1, t_0 + time_shift); // time shift here    
 	function_alphabeta->SetParameter (2, alpha);  
 	function_alphabeta->SetParameter (3, beta);  
+
+////////////
+	
+  // dummy pulse shape derivative
+  // calculate derivative with some method here precisely. 
+
+	vector <double> pulseShapeDerivative;
+	pulseShapeDerivative.push_back(0.0);
+	pulseShapeDerivative.push_back(0.0);
+	pulseShapeDerivative.push_back(0.05);
+	pulseShapeDerivative.push_back(0.1);
+	pulseShapeDerivative.push_back(0.35);
+	pulseShapeDerivative.push_back(0.25);
+	pulseShapeDerivative.push_back(-0.25);
+	pulseShapeDerivative.push_back(-0.25);
+	pulseShapeDerivative.push_back(-0.25);
+	pulseShapeDerivative.push_back(0.0);
+
+	int verbosity = 1;
+	int nSamples = 10;
+	double tMax = 4; // time of peak in samples 
+
+	// Create instance of object ComputeWeights
+
+	// (int verbosity, bool doFitBaseline, bool doFitTime, int nPulseSamples, int nPrePulseSamples)
+	ComputeWeights A_(verbosity, false, false, nSamples, 3);
+	//ComputeWeights A(1, false, false, 10, 3);
+
+	vector<double> pulseShape;
+	// pulse is moved to right by time_shift, so move sampling to right by time_shift.
+	int count_ = 0;
+
+	//cout << "A = " << A << "\n";
+
+	for(double i = xmin + time_shift; i < xmax + time_shift; i += dt){
+
+	  if ( i <= (t_0 + time_shift - alpha*beta) ) pulseShape.push_back(0);
+	  else pulseShape.push_back(function_alphabeta->Eval(i));
+	
+	  cout << "ideal weight sample = " << pulseShape[count_] << "\n";
+	  count_ += 1;
+	}
+
+      A_.compute(pulseShape,pulseShapeDerivative,tMax); // Run member function
+
+      if (verbosity > 0) {
+        cout << "verbosity_ = " << A_.GetVerbosity() << endl;
+        cout << "doFitBaseline_ = " << A_.GetDoFitBaseline() << endl;
+        cout << "doFitTime_ = " << A_.GetDoFitTime() << endl;
+        cout << "nPulseSamples_ = " << A_.GetnPulseSamples() << endl;
+        cout << "nPrePulseSamples_ = " << A_.GetnPrePulseSamples() << endl;
+
+        cout << "A.getAmpWeight(5) returns: " << A_.getAmpWeight(5) << endl;
+        cout << "A.getChi2Matrix(5,5) returns: " << A_.getChi2Matrix(5,5) << endl;
+      }
+
+////////////
 
 	int samp_number = 0;
 
@@ -54,6 +119,5 @@ double recon_amp(double A, double t_0, double time_shift, double alpha, double b
 	  }
 
 	return recon_amp;
-
 
 }
